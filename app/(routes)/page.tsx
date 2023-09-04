3e43245c6f1764657d0ebdf26a7a164aa0dbd762
tarek magdy <tarekmagdy21@outlook.com>
2023-09-04 13:25:39 +0300

    final

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,25 @@
+import getBillboards from "@/actions/get-billboards";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+export const revalidate = 0;
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboards("5f8417e2-01c4-4338-824f-3574e6c2c440");
+
+  return (
+    <div>
+      <Container>
+        <div className="space-y-10 pb-10">
+          <Billboard data={billboard} />
+        </div>
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default HomePage;
